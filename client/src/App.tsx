8d34512bef924f85a887234d61c16f0f0d529148
seantokuzo <seantokuzo@gmail.com>
2024-05-04 00:38:53 -0700

    basic client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage/LandingPage'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/store/userReducer/userReducer.ts
@@ -0,0 +1,38 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { LoginPayload } from './types/userTypes'
+
+interface UserState {
+  firstName: string
+  lastName: string
+  email: string
+  pfp: string | null
+}
+
+const initialState: UserState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  pfp: '',
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginUser: (state, action: PayloadAction<LoginPayload>): UserState => {
+      const { firstName, lastName, email, pfp } = action.payload
+      state.firstName = firstName || ''
+      state.lastName = lastName || ''
+      state.email = email || ''
+      state.pfp = pfp || null
+      return state
+    },
+    logoutUser: (state): UserState => {
+      state = initialState
+      return state
+    },
+  },
+})
+
+export const { loginUser, logoutUser } = userSlice.actions
+export default userSlice.reducer
