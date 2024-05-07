f29d74d2457bfd0fc8e6ffab1a1e35d522c2968e
seantokuzo <seantokuzo@gmail.com>
2024-05-07 14:45:15 -0700

    server updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,14 @@ const PORT = process.env.PORT || 3000
 
 const start = async () => {
   if (!process.env.JWT_SECRET) throw Error('❌ JWT_SECRET must be defined')
-  if (!process.env.JWT_LIFETIME) throw Error('❌ JWT_SECRET must be defined')
+  if (!process.env.JWT_LIFETIME) throw Error('❌ JWT_LIFETIME must be defined')
   if (!process.env.MONGO_URI) throw Error('❌ JWT_SECRET must be defined')
+  if (!process.env.GOOGLE_CLIENT_ID)
+    throw Error('❌ GOOGLE_CLIENT_ID must be defined')
+  if (!process.env.GOOGLE_CLIENT_SECRET)
+    throw Error('❌ GOOGLE_CLIENT_SECRET must be defined')
+  if (!process.env.GOOGLE_REDIRECT_URL)
+    throw Error('❌ GOOGLE_REDIRECT_URL must be defined')
 
   try {
     await mongoose.connect(process.env.MONGO_URI, {})
