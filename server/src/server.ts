3a2c38b640b78a54d31cb6ddaf8050eab8ff819f
seantokuzo <seantokuzo@gmail.com>
2024-05-08 21:27:32 -0700

    updates for env vars in docker

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import { DatabaseConnectionError } from './errors/database-connection-error'
 const PORT = process.env.PORT || 3000
 
 const start = async () => {
+  // CHECK FOR ENVIRONMENTAL VARIABLES
   if (!process.env.JWT_SECRET) throw Error('❌ JWT_SECRET must be defined')
   if (!process.env.JWT_LIFETIME) throw Error('❌ JWT_LIFETIME must be defined')
   if (!process.env.MONGO_URI) throw Error('❌ MONGO_URI must be defined')
@@ -14,6 +15,10 @@ const start = async () => {
     throw Error('❌ GOOGLE_CLIENT_SECRET must be defined')
   if (!process.env.GOOGLE_REDIRECT_URL)
     throw Error('❌ GOOGLE_REDIRECT_URL must be defined')
+  if (!process.env.AWS_ACCESS_KEY)
+    throw Error('❌ AWS_ACCESS_KEY must be defined')
+  if (!process.env.AWS_SECRET_ACCESS_KEY)
+    throw Error('❌ AWS_SECRET_ACCESS_KEY must be defined')
 
   try {
     await mongoose.connect(process.env.MONGO_URI, {})
