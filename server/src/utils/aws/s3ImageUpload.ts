b60e4d441386ec71a218d4373286169bdd3f6099
seantokuzo <seantokuzo@gmail.com>
2024-05-10 20:33:21 -0700

    update to aws-sdk v3

--- server/src/utils/aws/s3ImageUpload.ts
+++ server/src/utils/aws/s3ImageUpload.ts
@@ -1,37 +1,75 @@
-import AWS from 'aws-sdk'
+import {
+  PutObjectAclCommandOutput,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3'
+import { InternalError } from '../../errors/internal-error'
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const s3Client = new S3Client({
+  region: 'us-west-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
 })
 
-const s3 = new AWS.S3({ params: { Bucket: 'gogo-stelvis' } })
-
-export const s3ImageUpload = (params: any) => {
-  return new Promise((resolve, reject) => {
+// const params = {
+//   Bucket: 'gogo-stelvis',
+// }
+export const s3ImageUpload = async (params: any) => {
+  return new Promise<string>((resolve, reject) => {
     const { filename, file } = params
-
     const buf = Buffer.from(file.replace(/^data:.+;base64,/, ''), 'base64')
-
     const currentTime = new Date().getTime()
 
+    const url = `https://gogo-stelvis.s3.amazonaws.com/${currentTime}_${filename}`
+
     const data = {
+      // ACL: 'bucket-owner-read',
       Key: `${currentTime}_${filename}`,
       Body: buf,
       ContentEncoding: 'base64',
-      ACL: 'public-read',
       Bucket: 'gogo-stelvis',
     }
 
-    s3.putObject(data, (err, data) => {
+    const command = new PutObjectCommand(data)
+
+    s3Client.send(command, (err, data) => {
       if (err) {
-        console.log(`Error uploading file: ${err}`)
         reject(err)
       } else {
-        const url = `https://gogo-stelvis.s3.amazonaws.com/${currentTime}_${filename}`
-        console.log(`File uploaded successfully. File URL: ${url}`)
-        resolve({ url })
+        resolve(url)
       }
     })
   })
 }
+
+// * aws-sdk v2:
+// export const s3ImageUpload = (params: any) => {
+//   return new Promise((resolve, reject) => {
+//     const { filename, file } = params
+
+//     const buf = Buffer.from(file.replace(/^data:.+;base64,/, ''), 'base64')
+
+//     const currentTime = new Date().getTime()
+
+//     const data = {
+//       Key: `${currentTime}_${filename}`,
+//       Body: buf,
+//       ContentEncoding: 'base64',
+//       ACL: 'public-read',
+//       Bucket: 'gogo-stelvis',
+//     }
+
+//     s3.putObject(data, (err, data) => {
+//       if (err) {
+//         console.log(`Error uploading file: ${err}`)
+//         reject(err)
+//       } else {
+//         const url = `https://gogo-stelvis.s3.amazonaws.com/${currentTime}_${filename}`
+//         console.log(`File uploaded successfully. File URL: ${url}`)
+//         resolve({ url })
+//       }
+//     })
+//   })
+// }
