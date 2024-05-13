c131dc57bfafbd0cad808f18e9f494310272598f
seantokuzo <seantokuzo@gmail.com>
2024-05-13 00:07:42 -0700

    moduleDirectories point to test dir

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
   testEnvironment: 'jest-environment-jsdom',
   setupFiles: ['<rootDir>//src/test/setupFile.ts'],
   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
-  // moduleDirectories: ['node_modules', '<rootDir>/src/test'],
+  moduleDirectories: ['node_modules', '<rootDir>/src/test'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
