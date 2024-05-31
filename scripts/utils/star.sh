b2e8b814dbd62113ee1114b0ae686503fa11b200
seantokuzo <seantokuzo@gmail.com>
2024-05-30 21:59:37 -0700

    add eslint and helper pkgs - config - npm scripts

---
+++ scripts/utils/star.sh
@@ -0,0 +1,21 @@
+
+BR="\033[1;31m"
+BO="\033[1;38;5;208m"
+BY="\033[1;33m"
+BG="\033[1;32m"
+BB="\033[1;38;5;69m"
+BI="\033[1;38;5;75m"
+BV="\033[1;38;5;93m"
+BW='\033[1;37m'
+NC='\033[0m'
+
+# Example usage
+echo -e "${BY}           ®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
+echo -e "${BY}          ®®®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
+echo -e "${BY}         ®®®®®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
+echo -e "${BY}®®®®®®®®®®®®®®®®®®®®®®®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
+echo -e "${BY}    ®®®®®®®®®®®®®®®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
+echo -e "${BY}      ®®®®®®®®®®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
+echo -e "${BY}     ®®®®®® ®®®®®®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
+echo -e "${BY}    ®®®         ®®®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
+echo -e "${BY}   ®               ®${BO} ®${BR} ®${BV} ®${BI} ®${BG} ®${RESET}"
