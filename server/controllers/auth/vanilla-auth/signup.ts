21d43003110e22bc2758a5d1e4f1533a2a72cc85
seantokuzo <seantokuzo@gmail.com>
2024-06-25 20:19:20 -0700

    migrate to postgres

--- server/controllers/auth/vanilla-auth/signup.ts
+++ server/controllers/auth/vanilla-auth/signup.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import { ValidationError } from '../../../errors/validation-error';
 import { RequestValidationError } from '../../../errors/request-validation-error';
-import { User } from '../../../models/user';
 import { BadRequestError } from '../../../errors/bad-request-error';
 import { attachCookie } from '../../../utils/attachCookie';
 import { validateEmail } from '../../../utils/requestValidators/validateEmail';
 import { validatePassword } from '../../../utils/requestValidators/validatePassword';
+import { Password } from './utils/password';
+import pgPool from '../../../db/pgPool';
+import { createJwt } from './utils/createJwt';
 
 const signup = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -29,23 +31,37 @@ const signup = async (req: Request, res: Response) => {
   }
 
   // CHECK IF USER ALREADY EXISTS
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
+  const existingUserQuery = `
+    SELECT * FROM users WHERE email=$1
+  `;
+  const existingUserValues = [email];
+  const { rows: existingUserResult } = await pgPool.query(
+    existingUserQuery,
+    existingUserValues,
+  );
+  if (existingUserResult[0]) {
     throw new BadRequestError('User already exists');
   }
 
   // SIGN UP NEW USER
-  const newUser = User.build({
-    email,
-    password,
-    userType: 'gogo',
-  });
-  await newUser.save();
+  const hashedPass = await Password.hashPassword(password);
+  const signupQuery = `
+    INSERT INTO users (email, password)
+    VALUES($1, $2);
+  `;
+  const signupValues = [email, hashedPass];
+  const { rows: signupResult } = await pgPool.query(signupQuery, signupValues);
+  // const newUser = User.build({
+  //   email,
+  //   password,
+  //   userType: 'gogo',
+  // });
+  // await newUser.save();
 
-  const jwt = newUser.createJwt();
+  const jwt = createJwt(signupResult[0].id);
   attachCookie(res, jwt);
 
-  res.status(201).send(newUser);
+  res.status(201).send(signupResult[0]);
 };
 
 export default signup;
