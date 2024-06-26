e4f24c0724a10095477128c9d5823a1a9988c48c
seantokuzo <seantokuzo@gmail.com>
2024-06-25 19:09:15 -0700

    remove unused mongoose

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,10 @@
-// import mongoose from 'mongoose';
 import { app } from './app';
 import { checkEnvVars } from './utils/checkEnvVars';
-// import { DatabaseConnectionError } from './errors/database-connection-error';
 
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
   checkEnvVars();
-
-  // try {
-  //   await mongoose.connect(process.env.MONGO_URI, {});
-  //   console.log('🍃 Connected to the database');
-  // } catch (error) {
-  //   throw new DatabaseConnectionError();
-  // }
 
   app.listen(PORT, () => {
     console.log(`💥 App listening on port ${PORT}`);
