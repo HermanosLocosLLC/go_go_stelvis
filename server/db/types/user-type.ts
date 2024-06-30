90e563152febf1ae36bb08e77d0f24d77073d9ad
seantokuzo <seantokuzo@gmail.com>
2024-06-30 00:03:52 -0700

    fix types and temp change for bug

--- server/db/types/user-type.ts
+++ server/db/types/user-type.ts
@@ -7,10 +7,11 @@ export interface UserInterface {
   first_name?: string;
   last_name?: string;
   pfp?: string;
-  user_type: UserType;
+  user_type: string;
+  // user_type: UserType;
   access_token?: string;
   refresh_token?: string;
   is_active: boolean;
-  created_at: string;
-  updated_at: string;
+  created_at?: string;
+  updated_at?: string;
 }
