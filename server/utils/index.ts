081574e564befb667883ab033177a15ebf064126
seantokuzo <seantokuzo@gmail.com>
2024-06-30 23:45:43 -0700

    add date formatting class with static methods

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -2,3 +2,5 @@ export * from './checkEnvVars';
 export * from './aws/s3ImageUpload';
 export * from './email/email';
 export * from './requestValidators/validateEmail';
+export * from './decodeJwt';
+export * from './dateFormatter';
