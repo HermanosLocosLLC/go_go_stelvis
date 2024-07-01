0b964591a2fc1ca09b4b28c5955f4234b2398593
seantokuzo <seantokuzo@gmail.com>
2024-06-30 23:45:01 -0700

    update jwt sign and decode

--- server/controllers/auth/utils/createJwt.ts
+++ server/controllers/auth/utils/createJwt.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
 
-export const createJwt = (userId: string) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET!, {
-    expiresIn: process.env.JWT_LIFETIME,
+export const createJwt = (userId: string, confirmed = true) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET!, {
+    expiresIn: confirmed
+      ? process.env.JWT_LIFETIME
+      : process.env.JWT_CONFIRM_LIFETIME,
   });
-  return token;
 };
