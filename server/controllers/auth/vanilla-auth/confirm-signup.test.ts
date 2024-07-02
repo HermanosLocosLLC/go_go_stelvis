12b8caf618f38ea2b4ae8d39ff7d0b4663f1c297
seantokuzo <seantokuzo@gmail.com>
2024-07-01 20:51:07 -0700

    rename test

--- server/controllers/auth/vanilla-auth/confirm-signup.test.ts
+++ server/controllers/auth/vanilla-auth/confirm-signup.test.ts
@@ -3,7 +3,7 @@ import { app } from '../../../app';
 
 const gogoConfirmSignupUrl = '/api/v1/auth/gogo/confirm-signup';
 
-describe('🧪 Vanilla-Login Unit Tests 🧪', () => {
+describe('🧪 Vanilla Confirm Signup Unit Tests 🧪', () => {
   it('🧪 Fails if no valid request query token', async () => {
     const response = await request(app).post(gogoConfirmSignupUrl).send();
 
